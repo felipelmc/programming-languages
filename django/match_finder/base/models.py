a33d62bba4935c9f5ba8d665fc6272423864ed8d
Felipe Lamarca <felipelmc@pop-os.localdomain>
2021-11-30 22:00:21 -0300

    Drafting models

--- django/match_finder/base/models.py
+++ django/match_finder/base/models.py
@@ -1,15 +1,33 @@
 from django.db import models
 
-# Create your models here.
-"""class index(models.Model):
-    #host = 
-    #topic =
-    name = models.CharField(max_length=200)
-    description = models.TextField(null=True, blank=True)
-    #participants =
-    updated = models.DateTimeField(auto_now=True)
-    created = models.DateTimeField(auto_now_add=True)
-    def __str__(self):
-            return str(self.name)"""
+class Usuario(models.Model):
+    id_usuario = models.IntegerField()
+    primeiro_nome = models.CharField(max_length=80)
+    sobrenome = models.CharField(max_length=80)
+    email = models.EmailField(max_length=80)
+    username = models.CharField(max_length=15)
+    #senha =
+    CEP = models.IntegerField()
+    cidade = models.CharField(max_length=80)
+    estado = models.CharField(max_length=80)
+    status = models.CharField(max_length=150)
+    fav_esport = models.CharField(max_length=80)
+    
+    
+class Quadra(models.Model):
+    id_quadra = models.IntegerField()
+    tipo = models.CharField(max_length=80)
+    rua = models.CharField(max_length=80)
+    CEP = models.IntegerField()
+    cidade = models.CharField(max_length=80)
+    estado = models.CharField(max_length=80)
+    alugada = models.BooleanField(max_length=3) #Sim, Não
+    estrelas = models.IntegerField() # 1 - 5
+    valor_hora = models.IntegerField()
+    
+    
+    
+    
+
 
     
