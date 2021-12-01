d0c1233c0b91793656d88deaa3058b1f257b975d
guilherme-melo <guigui.melo2008@hotmail.com>
2021-12-01 19:36:11 -0300

    Creating user auth

--- django/match_finder/base/models.py
+++ django/match_finder/base/models.py
@@ -1,11 +1,41 @@
+from typing_extensions import Required
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
-class Usuario(models.Model):
+class UsuarioManager(BaseUserManager):
+    def create_user(self,username,email,telefone,primeiro_nome,sobrenome,password=None):
+        if not email:
+            raise ValueError('O email é obrigatório')
+        user = self.model(
+            username=username,
+            email=self.normalize_email(email),
+            telefone=telefone,
+            primeiro_nome=primeiro_nome,
+            sobrenome=sobrenome
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self,username,email,telefone,primeiro_nome,sobrenome,password=None):
+        user = self.create_user(
+            username=username,
+            email=email,
+            telefone=telefone,
+            primeiro_nome=  primeiro_nome,
+            sobrenome=sobrenome,
+            password=password
+        )
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+class Usuario(AbstractBaseUser):
     ip_usuario = models.GenericIPAddressField()
-    primeiro_nome = models.CharField(max_length=80)
-    sobrenome = models.CharField(max_length=80)
-    email = models.EmailField(max_length=80)
-    username = models.CharField(max_length=15)
+    primeiro_nome = models.CharField(max_length=80, verbose_name='primeiro_nome')
+    sobrenome = models.CharField(max_length=80,verbose_name='sobrenome')
+    email = models.EmailField(max_length=80,verbose_name='email', unique=True)
+    username = models.CharField(max_length=15,verbose_name='username', unique=True)
+    telefone = models.CharField(max_length=15,verbose_name='telefone', unique=True)
     #senha = 
     CEP = models.IntegerField()
     cidade = models.CharField(max_length=80)
@@ -16,8 +46,12 @@ class Usuario(models.Model):
     idade = models.IntegerField()
     peso = models.FloatField()
     genero = models.SmallIntegerField()
-    
-    
+    is_superuser = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['primeiro_nome', 'sobrenome', 'email', 'telefone']
+    objects = UsuarioManager()
+
 class Quadra(models.Model):
     tipo = models.CharField(max_length=80)
     rua = models.CharField(max_length=80)
