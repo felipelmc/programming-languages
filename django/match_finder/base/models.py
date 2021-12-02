3b07f7efe82f11cc73e766945ed216022300416a
guilherme-melo <guigui.melo2008@hotmail.com>
2021-12-01 21:01:43 -0300

    user auth done

--- django/match_finder/base/models.py
+++ django/match_finder/base/models.py
@@ -30,22 +30,22 @@ class UsuarioManager(BaseUserManager):
         user.save(using=self._db)
         return user
 class Usuario(AbstractBaseUser):
-    ip_usuario = models.GenericIPAddressField()
+    ip_usuario = models.GenericIPAddressField(max_length=45,null=True,blank=True)
     primeiro_nome = models.CharField(max_length=80, verbose_name='primeiro_nome')
     sobrenome = models.CharField(max_length=80,verbose_name='sobrenome')
     email = models.EmailField(max_length=80,verbose_name='email', unique=True)
     username = models.CharField(max_length=15,verbose_name='username', unique=True)
     telefone = models.CharField(max_length=15,verbose_name='telefone', unique=True)
     #senha = 
-    CEP = models.IntegerField()
+    CEP = models.CharField(max_length=8,verbose_name='CEP')
     cidade = models.CharField(max_length=80)
     estado = models.CharField(max_length=80)
     status = models.CharField(max_length=150)
     fav_esport = models.CharField(max_length=80)
-    altura = models.FloatField()
-    idade = models.IntegerField()
-    peso = models.FloatField()
-    genero = models.SmallIntegerField()
+    altura = models.IntegerField(default=0)
+    idade = models.IntegerField(default=0)
+    peso = models.FloatField(default=0)
+    genero = models.CharField(max_length = 1 ,default=0)
     is_superuser = models.BooleanField(default=False)
 
     USERNAME_FIELD = 'username'
