1c07a2e636f0d7e755dc3a5c17d2c7b11c00cda6
LucaEscopelli <luca.escopelli@gmail.com>
2021-12-02 18:54:06 -0300

    Corrigindo cabeçalho

--- django/match_finder/base/views.py
+++ django/match_finder/base/views.py
@@ -1,28 +1,37 @@
 from django.shortcuts import render
 
 def ranking(request):
-    return render(request, 'base/ranking.html')
+    content = {'name': 'RANKING'}
+    return render(request, 'base/ranking.html', content)
 
 def index(request):
-    return render(request, 'base/index.html')
+    content = {'name': 'INÍCIO'}
+    return render(request, 'base/index.html', content)
 
 def perfis(request):
-    return render(request, 'base/perfis.html')
+    content = {'name': 'DESENVOLVEDORES'}
+    return render(request, 'base/perfis.html', content)
     
 def perfil_usuario(request):
+    #content = {'name': 'PERFIL USUÁRIO'}
     return render(request, 'base/perfil_usuario.html')
 
 def projeto(request):
-    return render(request, 'base/projeto.html')
+    content = {'name': 'O PROJETO'}
+    return render(request, 'base/projeto.html', content)
 
 def registro(request):
+    #content = {'name': 'REGISTRO'}
     return render(request, 'base/registro.html')
 
 def mapa(request):
-    return render(request, 'base/mapa.html')
+    content = {'name': 'MAPA'}
+    return render(request, 'base/mapa.html', content)
 
 def chat(request):
-    return render(request, 'base/chat.html')
+    content = {'name': 'CHAT'}
+    return render(request, 'base/chat.html', content)
 
 def login(request):
+    #content = {'name': 'LOGIN'}
     return render(request, 'base/login.html')
