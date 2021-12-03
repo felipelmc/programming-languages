1787468c371b7ad62a1daae45e95629fd86fb74e
Janaina Neres <janaina.evangelistaneres@gmail.com>
2021-12-03 19:42:34 -0300

    db-sqlite

--- django/match_finder/match_finder/settings.py
+++ django/match_finder/match_finder/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ge',
     'base.apps.BaseConfig',
     'preco_quadras'
 ]
